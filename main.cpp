08ce5b52dfba16bd6fc99bb9d27eafac7c090e70
sosnin_roman <sosnin_roman@rambler.ru>
2017-09-10 20:04:29 +0500

    - Добавление операций сложения и вычитания - Редактирование кода - Исправление создания 1d nMatrix

--- main.cpp
+++ main.cpp
@@ -10,40 +10,28 @@ int main(int argc, char* argv[])
 	matrix[0][0][1] = 5;  
 	matrix[0][0][2] = 4;  
 
-	matrix.print();
-
 	matrix[0][1][0] = 3;
 	matrix[0][1][1] = 8;
 	matrix[0][1][2] = 12;
-
-	matrix.print();
 
 	matrix[0][2][0] = 1;  
 	matrix[0][2][1] = 4;  
 	matrix[0][2][2] = 5;  
 
-	matrix.print();
-
 	matrix[1][0][0] = 2;  
 	matrix[1][0][1] = 2;  
 	matrix[1][0][2] = 7;  
-
-	matrix.print();
 
 	matrix[1][1][0] = 8;
 	matrix[1][1][1] = 11;
 	matrix[1][1][2] = 3;
 
-	matrix.print();
-
 	matrix[1][2][0] = 1;  
 	matrix[1][2][1] = 2;  
 	matrix[1][2][2] = 5; 
 
-	matrix.print();
-
 	auto c = matrix[0][2][0];
-	std::cout << '\n';
+	/*std::cout << '\n';
 	std::cout << ".................. \n";
 
 	for(int i = 0; i < 2; ++i)
@@ -59,52 +47,67 @@ int main(int argc, char* argv[])
 		}
 		std::cout << '\n';
 		std::cout << "................... \n";
-	}
+	}*/
 	//
 
 	nMatrix<int, 3> matrix_copy_matrix(matrix);
-	matrix_copy_matrix.print();
-	std::cout << '\n' << matrix_copy_matrix.size() << " " << matrix_copy_matrix.get<0>() << " " << matrix_copy_matrix.get<1>() << " " << matrix_copy_matrix.get<2>();
-	std::cout << "\n \n";
+
+	matrix_copy_matrix[0] += 600;
+	//std::cout << '\n' << matrix_copy_matrix.size() << " " << matrix_copy_matrix.get<0>() << " " << matrix_copy_matrix.get<1>() << " " << matrix_copy_matrix.get<2>();
+	//std::cout << '\n';
 	//
 	nMatrix<double, 3> matrix_double_copy_matrix(matrix);
-	matrix_copy_matrix.print();
-	std::cout << "\n \n";
+
+	matrix_copy_matrix += 5; 
+
 	//
 	nMatrix<double,2> matrix_from_slice(matrix[1]);
-	matrix_from_slice.print();
-	std::cout << "\n \n";
+
 	//
 	nMatrix<int, 3> matrix_operation_eq(size);
 	matrix_operation_eq = matrix;
-	matrix_operation_eq.print();
-	std::cout << "\n \n";
+
 	//
 	size_t small_size[3] = {1,1,1};
 	nMatrix<int, 3> small_matrix(small_size);
 	nMatrix<int, 3> matrix_operation_eq2(matrix);
 	matrix_operation_eq2 = small_matrix;
-	matrix_operation_eq2.print();
-	std::cout << "\n \n";
+
 	//
 	nMatrix<int, 2> matrix_operation_eq_slice(size);
 	matrix_operation_eq_slice = matrix[0];
-	matrix_operation_eq_slice.print();
-	std::cout << "\n \n";
+
 	//
 	nMatrix<int, 1> one_dim_matrix(3);
 	one_dim_matrix[0] = 2;
 	one_dim_matrix[1] = 7;
 	one_dim_matrix[2] = 4;
-	const nMatrix<int, 1> const_matrix(one_dim_matrix);
-	const_matrix.print();
+	nMatrix<int, 1> const_matrix(one_dim_matrix);
+
 	one_dim_matrix[1] = 444;
 	//
 	const nMatrix<int, 3> const_matrix_copy_matrix(matrix);
 	int vvv = const_matrix_copy_matrix[0][0][0];
 	nMatrix<int,2> two_dim_matrix(const_matrix_copy_matrix[1]);
-	two_dim_matrix.print();
-	std::cout << "\n \n";
+
 	//
+	size_t d2[2] = {2,2};
+	nMatrix<int, 2> square_matrix(d2);
+	square_matrix[0][0] = 2;
+	square_matrix[0][1] = 5;
+	square_matrix[1][0] = 3;
+	square_matrix[1][1] = 8;
+	nMatrix<int, 2> square_matrix2(d2);
+	square_matrix2[0][0] = 1;
+	square_matrix2[0][1] = 1;
+	square_matrix2[1][0] = 1;
+	square_matrix2[1][1] = 1;
+
+	square_matrix += 100;
+
+	square_matrix -= 50;
+
+	square_matrix[0] += 600;
+
 	return 0;
 }
--- nMatrix.h
+++ nMatrix.h
@@ -1,11 +1,11 @@
 #include <valarray>
-#include <iostream>
 
 using std::valarray;
 using std::slice;
 
 template<class T, int N> class nMatrix;
 
+//Submatrix
 template<class T, int N>
 class SubMatrix
 {
@@ -13,22 +13,16 @@ public:
 	SubMatrix(slice sl, valarray<T>* vl, size_t* dim):
 		s(sl), v(vl), dim(dim){}
 	
-	SubMatrix<T, N-1> operator[] (size_t ind)
-	{
-		int shift = 1;
-		for(int i = 0; i < N-1; ++i)
-			shift *= dim[i];
-		return SubMatrix<T, N-1>(slice(ind * shift + s.start(), shift, 1), v, dim);
-	}
+	SubMatrix<T, N-1>			operator[] (size_t ind);
 
-	const SubMatrix<T, N-1> operator[] (size_t ind) const
-	{
-		int shift = 1;
-		for(int i = 0; i < N-1; ++i)
-			shift *= dim[i];
-		return SubMatrix<T, N-1>(slice(ind * shift + s.start(), shift, 1), v, dim);
-	}
+	const SubMatrix<T, N-1>		operator[] (size_t ind) const;	
 	
+	template<class AnotherType>
+	SubMatrix&					operator+= (const AnotherType& value);
+
+	template<class AnotherType>
+	SubMatrix&					operator-= (const AnotherType& value);
+
 	operator std::valarray<T>()
 	{
 		return valarray<T>( (*v)[s] );
@@ -37,13 +31,50 @@ public:
 	template<class Q, int P> friend class nMatrix;
 private:
 	SubMatrix();	
-	SubMatrix& operator=(const SubMatrix&);
+	SubMatrix&					operator=(const SubMatrix&);
 
-	valarray<T>* v;
-	slice s;
-	size_t* dim;
+	valarray<T>*				v;
+	slice						s;
+	size_t*						dim;
 };
 
+template<class T, int N>
+SubMatrix<T, N-1> SubMatrix<T, N>::operator[] (size_t ind)
+{
+	int shift = 1;
+	for(int i = 0; i < N-1; ++i)
+		shift *= dim[i];
+	return SubMatrix<T, N-1>(slice(ind * shift + s.start(), shift, 1), v, dim);
+}
+
+template<class T, int N>
+const SubMatrix<T, N-1>	SubMatrix<T, N>::operator[] (size_t ind) const
+{
+	int shift = 1;
+	for(int i = 0; i < N-1; ++i)
+		shift *= dim[i];
+	return SubMatrix<T, N-1>(slice(ind * shift + s.start(), shift, 1), v, dim);
+}
+
+template<class T, int N>
+template<class AnotherType>
+SubMatrix<T, N>& SubMatrix<T, N>::operator+= (const AnotherType& value)
+{	
+	for(size_t i = s.start(); i < s.size(); i=i+s.stride() )
+		v->operator[](i) += value;
+	return *this;
+}
+
+template<class T, int N>
+template<class AnotherType>
+SubMatrix<T, N>& SubMatrix<T, N>::operator-= (const AnotherType& value)
+{
+	for(size_t i = s.start(); i < s.size(); i=i+s.stride() )
+		v->operator[](i) -= value;
+	return *this;
+}
+
+//1d Submatrix specialization
 template<class T>
 class SubMatrix<T, 1>
 {
@@ -51,163 +82,246 @@ public:
 	SubMatrix(slice sl, valarray<T>* val, size_t* dim):
 		s(sl), v(val), dim(dim){}
 	
-	T& operator[] (size_t ind)
+	T&							operator[] (size_t ind)
 	{
 		return (*v)[s.start() + ind * s.stride()];
 	}
 
-	const T& operator[] (size_t ind) const
+	const T&					operator[] (size_t ind) const
 	{
 		return (*v)[s.start() + ind * s.stride()];
 	}
 	
+	template<class AnotherType>
+	SubMatrix&					operator+= (const AnotherType& value);
+
+	template<class AnotherType>
+	SubMatrix&					operator-= (const AnotherType& value);
+
 	operator valarray<T>()
 	{
 		return valarray<T>(v[s]);
 	}
 private:
 	SubMatrix();
-	SubMatrix& operator=(const SubMatrix&);
+	SubMatrix&					operator=(const SubMatrix&);
 	
-	valarray<T>* v;
-	slice s;
-	size_t* dim;
+	valarray<T>*				v;
+	slice						s;
+	size_t*						dim;
 };
 
+template<class T>
+template<class AnotherType>
+SubMatrix<T, 1>& SubMatrix<T, 1>::operator+= (const AnotherType& value)
+{	
+	for(size_t i = s.start(); i < s.size(); i=i+s.stride() )
+		v->operator[](i) += value;
+	return *this;
+}
+
+template<class T>
+template<class AnotherType>
+SubMatrix<T, 1>& SubMatrix<T, 1>::operator-= (const AnotherType& value)
+{
+	for(size_t i = s.start(); i < s.size(); i=i+s.stride() )
+		v->operator[](i) -= value;
+	return *this;
+}
+
+//nMatrix
 template<class T, int N>
 class nMatrix
 {
 public:
-	nMatrix(size_t* dimension):
-		sz(1)
-	{
-		for(int i = 0; i < N; ++i)
-		{
-			dim[N-1-i] = dimension[i];
-			sz *= dimension[i];
-		}
-		v = new valarray<T>(sz);
-	}
-	
+	nMatrix(size_t* dimension);
+
 	template<class AnotherType>
-	nMatrix(const SubMatrix<AnotherType,N>& rhs):
-		sz(1)
-	{
-		for(int i = 0; i < N; ++i)
-		{
-			dim[i] = rhs.dim[i];
-			sz *= dim[i];
-		}
-		v = new valarray<T>(sz);
-		for(size_t i = 0; i < sz; ++i)
-		{
-			int start = rhs.s.start();
-			v->operator[](i) = rhs.v->operator[](start + i);
-		}
-	}
-	
-	nMatrix(const nMatrix& rhs)
-	{
-		sz = rhs.sz;
-		v = new valarray<T>(sz);
-		for(int i = 0; i < N; ++i)
-		{
-			dim[i] = rhs.dim[i];
-		}		
-		slice sl(0, sz, 1);
-		v->operator[](sl) = *rhs.v;
-	}
-	
+	nMatrix(const SubMatrix<AnotherType,N>& rhs);
+
+	nMatrix(const nMatrix& rhs);
+
 	template<class AnotherType>
-	nMatrix(const nMatrix<AnotherType,N>& rhs)
-	{
-		sz = rhs.sz;
-		v = new valarray<T>(sz);
-		for(int i = 0; i < N; ++i)
-		{
-			dim[i] = rhs.dim[i];			
-			v->operator[](i) = rhs.v->operator[](i);
-		}
-	}
+	nMatrix(const nMatrix<AnotherType,N>& rhs);
 	
 	~nMatrix()
 	{
 		delete v;
 	}
 	
-	nMatrix& operator= (const nMatrix& rhs)
-	{
-		sz = rhs.sz;
-		v->resize(sz);
-		slice s(0, sz, 1);
-		v->operator[](s) = *rhs.v;
-		return *this;
-	}
+	nMatrix&					operator= (const nMatrix& rhs);
 
 	template<class AnotherType>
-	nMatrix& operator= (const nMatrix<AnotherType, N>& rhs)
-	{
-		sz = rhs.sz;
-		v->resize(sz);
-		slice s(0, sz, 1);
-		v->operator[](s) = *rhs->v;
-	}
+	nMatrix&					operator= (const nMatrix<AnotherType, N>& rhs);
 
 	template<class AnotherType>
-	nMatrix& operator= (const SubMatrix<AnotherType, N>& rhs)
-	{
-		sz = 1;
-		for(int i = 0; i < N; ++i)
-		{
-			dim[i] = rhs.dim[i];
-			sz *= dim[i];
-		}
-		v->resize(sz);
-		v->operator[](rhs.s) = *rhs.v;
-		return *this;
-	}
+	nMatrix&					operator= (const SubMatrix<AnotherType, N>& rhs);
 
-	SubMatrix<T, N-1> operator[] (size_t ind)
-	{
-		int shift = 1;
-		for(int i = 0; i < N-1; ++i)
-			shift *= dim[i];
-		return SubMatrix<T, N-1>(slice(ind*shift, shift, 1), v, dim);
-	}
+	template<class AnotherType>
+	nMatrix&					operator= (const AnotherType& value);
 
-	const SubMatrix<T, N-1> operator[] (size_t ind) const
-	{
-		int shift = 1;
-		for(int i = 0; i < N-1; ++i)
-			shift *= dim[i];
-		return SubMatrix<T, N-1>(slice(ind*shift, shift, 1), const_cast<valarray<T>*>(v), const_cast<size_t*>(dim));
-	}
+	template<class AnotherType>
+	nMatrix&					operator+= (const AnotherType& value);
 
-	void print() const
-	{
-		for(size_t i = 0; i < sz; ++i)
-			std::cout << (*v)[i] << " " ;
-		std::cout << '\n';
-	}
+	template<class AnotherType>
+	nMatrix&					operator-= (const AnotherType& value);
+
+	SubMatrix<T, N-1>			operator[] (size_t ind);
+
+	const SubMatrix<T, N-1>		operator[] (size_t ind) const;
 	
 	template<class Q, int P> friend class nMatrix;
 
-	size_t size() const
+	size_t						size() const
 	{
 		return sz;
 	}
 
 	template<int Dimension> 
-	size_t get() const
+	size_t						get() const
 	{
 		return dim[N - Dimension - 1];
 	}
 private:
-	valarray<T>* v;
-	size_t dim[N];
-	size_t sz;
+	valarray<T>*				v;
+	size_t						dim[N];
+	size_t						sz;
 };
 
+template<class T, int N>
+nMatrix<T,N>::nMatrix(size_t* dimension):
+		sz(1)
+{
+	for(int i = 0; i < N; ++i)
+	{
+		dim[N-1-i] = dimension[i];
+		sz *= dimension[i];
+	}
+	v = new valarray<T>(sz);
+}
+
+template<class T, int N>
+template<class AnotherType>
+nMatrix<T, N>::nMatrix(const SubMatrix<AnotherType,N>& rhs):
+	sz(1)
+{
+	for(int i = 0; i < N; ++i)
+	{
+		dim[i] = rhs.dim[i];
+		sz *= dim[i];
+	}
+	v = new valarray<T>(sz);
+	for(size_t i = 0; i < sz; ++i)
+	{
+		int start = rhs.s.start();
+		v->operator[](i) = rhs.v->operator[](start + i);
+	}
+}
+
+template<class T, int N>
+nMatrix<T, N>::nMatrix(const nMatrix<T, N>& rhs)
+{
+	sz = rhs.sz;
+	v = new valarray<T>(sz);
+	for(int i = 0; i < N; ++i)
+	{
+		dim[i] = rhs.dim[i];
+	}		
+	slice sl(0, sz, 1);
+	v->operator[](sl) = *rhs.v;
+}
+
+template<class T, int N>
+template<class AnotherType>
+nMatrix<T, N>::nMatrix(const nMatrix<AnotherType,N>& rhs)
+{
+	sz = rhs.sz;
+	v = new valarray<T>(sz);
+	for(int i = 0; i < N; ++i)
+	{
+		dim[i] = rhs.dim[i];			
+		v->operator[](i) = rhs.v->operator[](i);
+	}
+}
+
+template<class T, int N>
+nMatrix<T, N>&	nMatrix<T, N>::operator= (const nMatrix<T, N>& rhs)
+{
+	sz = rhs.sz;
+	v->resize(sz);
+	slice s(0, sz, 1);
+	v->operator[](s) = *rhs.v;
+	return *this;
+}
+
+template<class T, int N>
+template<class AnotherType>
+nMatrix<T, N>&	nMatrix<T, N>::operator= (const nMatrix<AnotherType, N>& rhs)
+{
+	sz = rhs.sz;
+	v->resize(sz);
+	slice s(0, sz, 1);
+	v->operator[](s) = *rhs->v;
+}
+
+template<class T, int N>
+template<class AnotherType>
+nMatrix<T, N>&	nMatrix<T, N>::operator= (const SubMatrix<AnotherType, N>& rhs)
+{
+	sz = 1;
+	for(int i = 0; i < N; ++i)
+	{
+		dim[i] = rhs.dim[i];
+		sz *= dim[i];
+	}
+	v->resize(sz);
+	v->operator[](rhs.s) = *rhs.v;
+	return *this;
+}
+
+template<class T, int N>
+template<class AnotherType>
+nMatrix<T, N>&	nMatrix<T, N>::operator= (const AnotherType& value)
+{
+	v->operator=(value);
+	return *this;
+}
+
+template<class T, int N>
+template<class AnotherType>
+nMatrix<T, N>&	nMatrix<T, N>::operator+= (const AnotherType& value)
+{		
+	v->operator+=(value);
+	return *this;
+}
+
+template<class T, int N>
+template<class AnotherType>
+nMatrix<T, N>&	nMatrix<T, N>::operator-= (const AnotherType& value)
+{
+	v->operator-=(value);
+	return *this;
+}
+
+template<class T, int N>
+SubMatrix<T, N-1> nMatrix<T, N>::operator[] (size_t ind)
+{
+	int shift = 1;
+	for(int i = 0; i < N-1; ++i)
+		shift *= dim[i];
+	return SubMatrix<T, N-1>(slice(ind*shift, shift, 1), v, dim);
+}
+
+template<class T, int N>
+const SubMatrix<T, N-1>	nMatrix<T, N>::operator[] (size_t ind) const
+{
+	int shift = 1;
+	for(int i = 0; i < N-1; ++i)
+		shift *= dim[i];
+	return SubMatrix<T, N-1>(slice(ind*shift, shift, 1), const_cast<valarray<T>*>(v), const_cast<size_t*>(dim));
+}
+
+//1d specialization
 template<class T>
 class nMatrix<T, 1>
 {
@@ -217,40 +331,180 @@ public:
 	{
 		v = new valarray<T>(dim);
 	}
+
+	template<class AnotherType>
+	nMatrix(const SubMatrix<AnotherType, 1>& rhs);
+
+	nMatrix(const nMatrix& rhs);
+
+	template<class AnotherType>
+	nMatrix(const nMatrix<AnotherType, 1>& rhs);
 	
 	~nMatrix()
 	{
 		delete v;
 	}
 	
-	T& operator[] (size_t ind)
+	T&							operator[] (size_t ind)
 	{
 		return (*v)[ind];
 	}
 
-	const T& operator[] (size_t ind) const
+	const T&					operator[] (size_t ind) const
 	{
 		return (*v)[ind];
 	}
 
-	void print() const
-	{
-		for(size_t i = 0; i < dim; ++i)
-			std::cout << (*v)[i] << " " ;
-		std::cout << '\n';
-	}
+	nMatrix&					operator= (const nMatrix& rhs);
 
-	size_t size() const
+	template<class AnotherType>
+	nMatrix&					operator= (const nMatrix<AnotherType, 1>& rhs);
+
+	template<class AnotherType>
+	nMatrix&					operator= (const AnotherType& value);
+
+	template<class AnotherType>
+	nMatrix&					operator= (const SubMatrix<AnotherType, 1>& rhs);
+
+	template<class AnotherType>
+	nMatrix&					operator+= (const AnotherType& value);
+
+	template<class AnotherType>
+	nMatrix&					operator-= (const AnotherType& value);
+
+	size_t						size() const
 	{
 		return dim;
 	}
 
 	template<int Dimension> 
-	size_t get() const
+	size_t						get() const
 	{
 		return dim;
 	}
 private:
-	valarray<T>* v;
-	size_t dim;
+	valarray<T>*				v;
+	size_t						dim;
 };
+
+template<class T>
+template<class AnotherType>
+nMatrix<T, 1>::nMatrix(const SubMatrix<AnotherType, 1>& rhs):
+	sz(1)
+{
+	dim = rhs.dim[0];
+	v = new valarray<T>(dim);
+	for(size_t i = 0; i < dim; ++i)
+	{
+		int start = rhs.s.start();
+		v->operator[](i) = rhs.v->operator[](start + i);
+	}
+}
+
+template<class T>
+nMatrix<T, 1>::nMatrix(const nMatrix<T, 1>& rhs)
+{
+	dim = rhs.dim;
+	v = new valarray<T>(dim);		
+	slice sl(0, dim, 1);
+	v->operator[](sl) = *rhs.v;
+}
+
+template<class T>
+template<class AnotherType>
+nMatrix<T, 1>::nMatrix(const nMatrix<AnotherType, 1>& rhs)
+{
+	dim = rhs.dim;
+	for(int i = 0; i < dim; ++i)
+	{					
+		v->operator[](i) = rhs.v->operator[](i);
+	}
+}
+
+template<class T>
+nMatrix<T, 1>&	nMatrix<T, 1>::operator= (const nMatrix<T, 1>& rhs)
+{
+	dim = rhs.dim;
+	v->resize(dim);
+	slice s(0, dim, 1);
+	v->operator[](s) = *rhs.v;
+	return *this;
+}
+
+template<class T>
+template<class AnotherType>
+nMatrix<T, 1>&	nMatrix<T, 1>::operator= (const nMatrix<AnotherType, 1>& rhs)
+{
+	dim = rhs.dim;
+	v->resize(dim);
+	slice s(0, dim, 1);
+	v->operator[](s) = *rhs->v;
+}
+
+template<class T>
+template<class AnotherType>
+nMatrix<T, 1>&	nMatrix<T, 1>::operator= (const AnotherType& value)
+{
+	v->operator=(value);
+	return *this;
+}
+
+template<class T>
+template<class AnotherType>
+nMatrix<T, 1>&	nMatrix<T, 1>::operator= (const SubMatrix<AnotherType, 1>& rhs)
+{
+	dim = rhs.dim;
+	v->resize(dim);
+	v->operator[](rhs.s) = *rhs.v;
+	return *this;
+}
+
+template<class T>
+template<class AnotherType>
+nMatrix<T, 1>&	nMatrix<T, 1>::operator+= (const AnotherType& value)
+{
+	v->operator+=(value);
+	return *this;
+}
+
+template<class T>
+template<class AnotherType>
+nMatrix<T, 1>&	nMatrix<T, 1>::operator-= (const AnotherType& value)
+{
+	v->operator-=(value);
+	return *this;
+}
+
+
+//Global functions
+template<class T, class AnotherType, int N>
+nMatrix<T,N> operator+(const nMatrix<T,N>& lhs, const AnotherType& rhs)
+{
+	nMatrix<T,N> result(lhs);
+	result += rhs;
+	return result;
+}
+
+template<class T, class AnotherType, int N>
+nMatrix<T,N> operator-(const nMatrix<T,N>& lhs, const AnotherType& rhs)
+{
+	nMatrix<T,N> result(lhs);
+	result -= rhs;
+	return result;
+}
+
+template<class T, class AnotherType, int N>
+nMatrix<T,N> operator+(const AnotherType& lhs, const nMatrix<T,N>& rhs)
+{
+	nMatrix<T,N> result(rhs);
+	result += lhs;
+	return result;
+}
+
+template<class T, class AnotherType, int N>
+nMatrix<T,N> operator-(const AnotherType& lhs, const nMatrix<T,N>& rhs)
+{
+	nMatrix<T,N> result(rhs);
+	result -= lhs;
+	return result;
+}
